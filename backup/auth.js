d87ebc6e7c4fd5fafb534fa08e8ac3b119a7644f
roman garms <romangarms@gmail.com>
2022-09-28 10:14:52 -0700

    Delete Unused Components

--- backup/auth.js
+++
@@ -1,7 +0,0 @@
-client_id = "---";
-redirect_uri = 'http://192.168.86.20:5000/login-success';
-scopes = "user-read-currently-playing";
-
-function login() {
-  location.href=`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.scopes}&show_dialog=true`
-}
