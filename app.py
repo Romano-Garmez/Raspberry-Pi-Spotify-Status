127c9b61298471c630ec75ab9d1075e8b334371b
roman garms <romangarms@gmail.com>
2023-11-07 13:56:37 -0800

    formatting fix

--- app.py
+++ app.py
@@ -24,27 +24,28 @@ import sys
 import re
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(64)
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = './.flask_session/'
+app.config["SECRET_KEY"] = os.urandom(64)
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_FILE_DIR"] = "./.flask_session/"
 Session(app)
 maxTitleLength = 25
 maxArtistLength = 35
 maxAlbumLength = 20
 
 
-@app.route('/')
+@app.route("/")
 def index():
-
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private user-modify-playback-state user-library-read user-library-modify playlist-modify-private playlist-modify-public',
-                                               cache_handler=cache_handler,
-                                               show_dialog=False)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        scope="user-read-currently-playing playlist-modify-private user-modify-playback-state user-library-read user-library-modify playlist-modify-private playlist-modify-public",
+        cache_handler=cache_handler,
+        show_dialog=False,
+    )
 
     if request.args.get("code"):
         # Step 2. Being redirected from Spotify auth page
         auth_manager.get_access_token(request.args.get("code"))
-        return redirect('/')
+        return redirect("/")
 
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         # Step 1. Display sign in link when no token
@@ -52,16 +53,16 @@ def index():
         return render_template("login.html", auth_url=auth_url)
 
     # Step 3. Signed in, display currently playing
-    return redirect('/currently_playing')
+    return redirect("/currently_playing")
 
 
-@app.route('/sign_out')
+@app.route("/sign_out")
 def sign_out():
     session.pop("token_info", None)
-    return redirect('/')
+    return redirect("/")
 
 
-@app.route('/currently_playing')
+@app.route("/currently_playing")
 def currently_playing():
     spotify = getSpotify()
     track = spotify.current_user_playing_track()
@@ -76,13 +77,24 @@ def currently_playing():
         id = track["item"]["id"]
         liked = spotify.current_user_saved_tracks_contains(tracks=[id])[0]
         currently_playing = track["is_playing"]
-        return render_template("currently_playing.html", title=title, artist=artist, album=album, art_url=art_url, id=id, currently_playing=currently_playing, liked=liked, json=json.dumps(track, indent=2))
+        return render_template(
+            "currently_playing.html",
+            title=title,
+            artist=artist,
+            album=album,
+            art_url=art_url,
+            id=id,
+            currently_playing=currently_playing,
+            liked=liked,
+            json=json.dumps(track, indent=2),
+        )
     return render_template("not_playing.html")
+
 
 # pinged every ~2 sec to see if refresh of page is required
 
 
-@app.route('/current_track_xhr')
+@app.route("/current_track_xhr")
 def current_track_xhr():
     spotify = getSpotify()
     track = spotify.current_user_playing_track()
@@ -90,17 +102,18 @@ def current_track_xhr():
         new_id = track["item"]["id"]
         id = request.args.get("id")
         new_currently_playing = track["is_playing"]
-        currently_playing = request.args.get("currently_playing") == 'True'
+        currently_playing = request.args.get("currently_playing") == "True"
         if id == new_id and currently_playing == new_currently_playing:
             return "same"
         else:
             return "different"
     new_currently_playing = False
-    currently_playing = request.args.get("currently_playing") == 'True'
+    currently_playing = request.args.get("currently_playing") == "True"
     if currently_playing == new_currently_playing:
         return "same"
     else:
         return "different"
+
 
 # end of html pages, now just assorted methods
 
@@ -112,48 +125,48 @@ def get_artists(artists_json):
     return ", ".join(artists)
 
 
-@app.route('/play')
+@app.route("/play")
 def play():
     spotify = getSpotify()
     spotify.start_playback()
-    return redirect('/currently_playing')
+    return redirect("/currently_playing")
 
 
-@app.route('/pause')
+@app.route("/pause")
 def pause():
     spotify = getSpotify()
     spotify.pause_playback()
-    return redirect('/currently_playing')
+    return redirect("/currently_playing")
 
 
-@app.route('/skip')
+@app.route("/skip")
 def skip():
     spotify = getSpotify()
     spotify.next_track()
-    return redirect('/currently_playing')
+    return redirect("/currently_playing")
 
 
-@app.route('/like')
+@app.route("/like")
 def like():
     spotify = getSpotify()
     id = request.args.get("id")
     spotify.current_user_saved_tracks_add(tracks=[id])
-    return redirect('/currently_playing')
+    return redirect("/currently_playing")
 
 
-@app.route('/unlike')
+@app.route("/unlike")
 def unlike():
     spotify = getSpotify()
     id = request.args.get("id")
     spotify.current_user_saved_tracks_delete(tracks=[id])
-    return redirect('/currently_playing')
+    return redirect("/currently_playing")
 
 
 def getSpotify():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        return redirect('/')
+        return redirect("/")
     return spotipy.Spotify(auth_manager=auth_manager)
 
 
@@ -162,7 +175,7 @@ def shortenText(string, length):
 
 
 def formatTitle(title):
-    title = re.sub('\(feat\. .+\)', '', title)
+    title = re.sub("\(feat\. .+\)", "", title)
     if len(title) > maxTitleLength:
         title = shortenText(title, maxTitleLength)
     return title
@@ -177,23 +190,24 @@ def formatArtist(artist):
 def formatAlbum(title, album):
     if len(album) > maxAlbumLength:
         if album == title:
-            return ''
+            return ""
         else:
             return shortenText(album, maxAlbumLength)
     return album
 
 
-'''
+"""
 Following lines allow application to be run more conveniently with
 `python app.py` (Make sure you're using python3)
-'''
-if __name__ == '__main__':
-    if os.getenv('SPOTIPY_CLIENT_ID') == None:
+"""
+if __name__ == "__main__":
+    if os.getenv("SPOTIPY_CLIENT_ID") == None:
         sys.exit("Missing Environment Variable: SPOTIPY_CLIENT_ID")
-    if os.getenv('SPOTIPY_CLIENT_SECRET') == None:
+    if os.getenv("SPOTIPY_CLIENT_SECRET") == None:
         sys.exit("Missing Environment Variable: SPOTIPY_CLIENT_SECRET")
-    if os.getenv('SPOTIPY_REDIRECT_URI') == None:
+    if os.getenv("SPOTIPY_REDIRECT_URI") == None:
         sys.exit("Missing Environment Variable: SPOTIPY_REDIRECT_URI")
     print(os.getenv("SPOTIPY_REDIRECT_URI"))
     from waitress import serve
+
     serve(app, host="0.0.0.0", port=8100)
